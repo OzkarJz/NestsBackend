d891c311f5b50e5a169934ad941208e74db50c90
Oscar Jimenez <ojimenezj1@miumg.edu.gt>
2023-09-30 23:04:44 -0600

    Prueba inicial

---
+++ ejemplo-backend/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ClientModule } from './client/client.module';
+
+@Module({
+  imports: [ClientModule],
+  controllers: [],
+  providers: [],
+  exports: []
+})
+export class AppModule {}
